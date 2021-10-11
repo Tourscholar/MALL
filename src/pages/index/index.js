67214056af003141b636d810343242eb4da6d32a
Tourscholar <krj1608178365@gmail.com>
2021-10-11 18:25:01 +0900

    add jjzyx components

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,14 +1,8 @@
 // css
-// import './css/reset.css';
+import './css/reset.css';
 import './css/bundle.css';
 
-// 公共样式
-import 'styles/reset.css';
-import 'styles/base.css';
 import 'styles/layout.css';
-
-// 页面样式
-import './index.css';
 
 // js
 import './js/menu';
