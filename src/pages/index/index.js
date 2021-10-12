8b541f680a267252a4c7975dc05d5cdb1d237f84
Tourscholar <krj1608178365@gmail.com>
2021-10-12 13:02:26 +0900

    add slide components

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,8 +1,14 @@
 // css
-import './css/reset.css';
+// import './css/reset.css';
 import './css/bundle.css';
 
+// 公共样式
+import 'styles/reset.css';
+import 'styles/base.css';
 import 'styles/layout.css';
+
+// 页面样式
+import './index.css';
 
 // js
 import './js/menu';
@@ -11,5 +17,6 @@ import './js/backtotop';
 
 // 组件
 import 'components/topbar';
-import './components/carousel';
+// import './components/carousel';
+import './components/slider';
 import './components/jjzyx';
